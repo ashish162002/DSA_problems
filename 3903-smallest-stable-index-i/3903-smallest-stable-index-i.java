812e72a578274a565529039a2567a843225e4fa1
Ashish Rajak <105159464+ashish162002@users.noreply.github.com>
2026-09-04 21:41:21 +0530

    Time: 1 ms (99.79%), Space: 46.4 MB (49.36%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -7,27 +7,21 @@ class Solution {
         int[] prefixMax = new int[n];
         int[] suffixMin = new int[n];
 
-        // Prefix maximum
-        prefixMax[0] = nums[0];
+      prefixMax[0]=nums[0];
+      for(int i = 1 ; i<n ; i++){
+    prefixMax[i]=Math.max(prefixMax[i-1],nums[i]);
+      }
+      
+       suffixMin[nums.length-1] = nums[nums.length -1] ;
 
-        for (int i = 1; i < n; i++) {
-            prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
-        }
-
-        // Suffix minimum
-        suffixMin[n - 1] = nums[n - 1];
-
-        for (int i = n - 2; i >= 0; i--) {
-            suffixMin[i] = Math.min(suffixMin[i + 1], nums[i]);
-        }
-
-        // Find smallest stable index
-        for (int i = 0; i < n; i++) {
+       for(int i =  n-2 ; i>=0 ; i--){
+   suffixMin[i]=Math.min(suffixMin[i+1],nums[i]);
+      }
+      for (int i = 0; i < n; i++) {
             if (prefixMax[i] - suffixMin[i] <= k) {
                 return i;
             }
         }
-
         return -1;
     }
 }
